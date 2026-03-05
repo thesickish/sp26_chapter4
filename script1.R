4e0d393960a7d2b6eef00465629b3d6b6a53b32b
Shmargad <yotam@arizona.edu>
2026-03-05 10:21:13 -0700

    update first script

---
+++ script1.R
@@ -0,0 +1,47 @@
+
+# Working with factors
+p <- c("Democrat","Democrat","Republican","Republican")
+p2 <- factor(p)
+
+levels(p)
+levels(p2)
+
+factor(p2,levels = c("Democrat","Republican","Independent"))
+factor(p2,levels = c("democrat","republican"))
+
+# Multidimensional data
+
+bad = cbind(c1 = c(10,5), c2 = c("a","b"))
+bad
+class(bad)
+
+good = data.frame(c1 = c(10,5), c2 = c("a","b"))
+good
+class(good)
+
+# Operating on data.frames
+
+# subsetting [row,col]
+good[1,1]
+good[1,]
+good[,1]
+
+good[order(good$c1)] # not what we want if sorting data
+good[order(good$c1),]
+
+# indexing from the right
+good[[-1]] # same as good[[2]]
+good[[-2]] # same as good[[1]]
+
+# subassignment
+good$c1[good$c1 < 10] <- 0
+good$c2[good$c1 < 10] <- "c"
+
+
+
+
+
+
+
+
+
